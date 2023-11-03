da7dee99306e06701565582bd37cf293e6593ed3
Monika Jaworowska <mon.jaworowska@gmail.com>
2023-11-03 13:53:30 +0100

    first commit

---
+++ radioStations.js
@@ -0,0 +1,104 @@
+const radioStations = [
+  {
+    id: 0,
+    name: "Polskie Radio Bialystok",
+    src: "http://stream4.nadaje.com:15476/radiobialystok",
+  },
+  {
+    id: 1,
+    name: "Radio ZET Chilli",
+    src: "https://ch.cdn.eurozet.pl/chi-net.mp3",
+  },
+  {
+    id: 2,
+    name: "Radio Strefa FM",
+    src: "http://play.strefa.fm:8000/strefa.ogg",
+  },
+  {
+    id: 3,
+    name: "Super Nova Trzebnica",
+    src: "http://ic1.smcdn.pl/1510-1.mp3",
+  },
+  {
+    id: 4,
+    name: "Fitness Music",
+    src: "https://s3.slotex.pl/shoutcast/7060/stream",
+  },
+  {
+    id: 5,
+    name: "Eska Zamość 128",
+    src: "https://ic2.smcdn.pl/2230-1.mp3",
+  },
+  {
+    id: 6,
+    name: "Eska Rock",
+    src: "https://ic2.smcdn.pl/5380-1.mp3#ESKA_ROCK",
+  },
+  {
+    id: 7,
+    name: "Bonton Chełm",
+    src: "http://streaming.inten.pl:8022/bonton.mp3",
+  },
+  {
+    id: 8,
+    name: "Antyradio",
+    src: "https://n-4-4.dcs.redcdn.pl/sc/o2/Eurozet/live/antyradio.livx",
+  },
+  {
+    id: 9,
+    name: "OpenFM Muzyka Filmowa",
+    src: "http://stream.open.fm/38",
+  },
+  {
+    id: 10,
+    name: "OpenFM Beats & Bass",
+    src: "http://stream.open.fm/106",
+  },
+  {
+    id: 11,
+    name: "OpenFM Love",
+    src: "http://stream.open.fm/121",
+  },
+  {
+    id: 12,
+    name: "OpenFM Lejdis Café",
+    src: "http://stream.open.fm/48",
+  },
+  {
+    id: 13,
+    name: "OpenFM 500 Heavy Hits",
+    src: "http://stream.open.fm/54",
+  },
+  {
+    id: 14,
+    name: "OpenFM 500 Electronic Hits",
+    src: "http://stream.open.fm/94",
+  },
+  {
+    id: 15,
+    name: "OpenFM 1001 Hits",
+    src: "http://stream.open.fm/11",
+  },
+  {
+    id: 16,
+    name: "OpenFM 100% Hits",
+    src: "http://stream.open.fm/64",
+  },
+  {
+    id: 17,
+    name: "Radio Złote Przeboje",
+    src: "http://stream.open.fm/201",
+  },
+  {
+    id: 18,
+    name: "OpenFM Rocks",
+    src: "https://stream.open.fm/77",
+  },
+  {
+    id: 19,
+    name: "Radio Kolor",
+    src: "http://stream.radiojar.com/xt0cf1v8938uv",
+  },
+];
+
+export { radioStations };
